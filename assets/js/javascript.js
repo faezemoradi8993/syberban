2d379e9869125a056982aed008c474106316195e
faeze moradi <faeze.moradi8993@gmail.com>
2021-05-13 04:19:15 +0430

    some changes

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -52,11 +52,6 @@ $bside.mouseup( function(e){
   $('html, body').css('overflow','auto'); 
  });
 
-
- /* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
-
-
-
   $('.dropdownbtn').click( function() {
     if(!$(this).parent().parents().hasClass('active')){
       $('.item').removeClass('active');    
